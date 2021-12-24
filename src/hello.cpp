cb582320e1b86e39949a61f5608c36586f8290ba
Jakob Flierl <jakob.flierl@gmail.com>
2021-12-24 20:03:08 +0100

    Fix hello world string.

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,19 +1,15 @@
 #include <QApplication>
 #include <QPushButton>
 
-#include <QDebug>
-
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-
-    qDebug() << "Hello World";
 
     QWidget window;
     window.setFixedSize(200, 50);
 
     QFont font ("Courier");
     
-    QPushButton *button = new QPushButton("Hello World", &window);
+    QPushButton *button = new QPushButton("Hello world!", &window);
     button->setGeometry(10, 10, 180, 30);
     button->setToolTip("A tooltip");
     button->setFont(font);
